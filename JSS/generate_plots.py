203e49bc6a740a08bfbc3ae72d89c9cc763a7a69
kenghweeng <ngkenghwee@hotmail.com>
2021-11-21 03:09:10 +0800

    Added env

---
+++ JSS/generate_plots.py
@@ -0,0 +1,19 @@
+import datetime
+import pandas as pd
+import gym
+import numpy as np
+import plotly.figure_factory as ff
+from pathlib import Path
+import sys
+import pickle
+
+'''
+We want to generate a Gantt chart based on the solution found inside the "/solutions" directory
+'''
+def create_job_matrix(fname):
+    # takes in a text file from "/instances" folder and generates job matrix
+    
+def render(instance, 
+
+_, instance = sys.argv
+
